7f8022c7f5f8b7cb9c5e967aeb6b2bac6031e6f6
wonderDevs2023 <150029099+wonderDevs2023@users.noreply.github.com>
2023-12-09 18:02:15 +0800

    Added Endpoint
    addCustomer added,  added cors to appts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
+import cors from 'cors';
 import { loginRouter,
          logOutRouter,
          addUserRouter, 
          findUsersRouter,
-         updateDailyCustomerRouter
+         updateDailyCustomerRouter,
+         addCustomerRouter
 } from "./routes";
 
 const app = express();
 const PORT: number = 3300;
 
 // middleware
+app.use(cors())
+app.use(express.json());
 
 
 // endpoint
@@ -18,6 +22,7 @@ app.use('/logout',logOutRouter);
 app.use('/addUser',addUserRouter);
 app.use('/findUser',findUsersRouter);
 app.use('/updateDailyCustomer',updateDailyCustomerRouter);
+app.use('/addCustomer', addCustomerRouter)
 
 app.listen( PORT, () => {
     console.log(`Server is running on port ${PORT}`)
